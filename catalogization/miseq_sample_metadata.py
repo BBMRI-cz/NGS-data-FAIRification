4706f9bd0b2f4207c4deec277a97005eb873b8d3
RadoslavaKacova <116794379+RadoslavaKacova@users.noreply.github.com>
2023-08-09 19:21:56 +0200

    Update miseq_sample_metadata.py
    script collects all parameters for module Material

--- catalogization/miseq_sample_metadata.py
+++ catalogization/miseq_sample_metadata.py
@@ -7,8 +7,8 @@ class SampleInfoMMCI:
 
     def __init__(self):
         self.idSample: str = ''          #clinical_info.json (pseudoID: "mmci_predictive_44d8480d-d2d4-44f9-aefe-7ace74329f7a")
-        self.collFromPerson: str = ''
-        self.belToDiag: str = ''
+        self.collFromPerson: str = '' #clinical_info.json (ID:mmci_patient_a8236c65-51c1-4626-b866-fbf9a79ca176
+        self.belToDiag: str = ''#clinical_info.json (ID:mmci_patient_a8236c65-51c1-4626-b866-fbf9a79ca176
         self.samplTimestamp: str = '' #clinical_info.json - takingDate
         self.regTimestamp: str = ''  # clinical_info.json - freezeTime
         self.bioSpeciType: str = '' #clinical_info.json - material
@@ -38,11 +38,42 @@ def find_el_in_statinfo(txt_statInfo, sample): #this function extracts run param
 def find_el_in_clinical_info(json_clinical_data, sample):  #
     with open(json_clinical_data) as json_file:
         data = json.load(json_file)
-    sample.idSample = data.get('clinical_data')[0].get('Samples')[0].get('pseudoID')
-    sample.collFromPerson = data.get('clinical_data')[0].get('ID')
-    sample.samplTimestamp = data.get('clinical_data')[0].get('Samples')[0].get('takingDate')
-    #sample.regTimestamp = data.get('clinical_data')[0].get('Samples')[0].get('freezeTime')
-    #sample.bioSpeciType =
+    sample.idSample = data.get('Samples')[0].get('pseudoID')
+    sample.collFromPerson = data.get('ID')
+    sample.belToDiag = sample.collFromPerson[:5] + "clinical" + sample.collFromPerson[12:]
+    material = data.get('Samples')[0].get('materialType')
+    bbm_module = data.get('Samples')[0].get('material')
+    if bbm_module == "tissue":
+        sample.samplTimestamp = data.get('Samples')[0].get('cutTime')
+        sample.regTimestamp = data.get('Samples')[0].get('freezeTime')
+    else:
+        sample.samplTimestamp = data.get('Samples')[0].get('takingDate')
+    sample.pathoState = "NoInformation (NI, nullflavor)"
+    if material == "4" or material == "54":
+        sample.pathoState = "Normal"
+    if material == "1" or material == "2" or material == "3" or material == "5" or material == "53" or material == "55" or material == "56":
+        sample.pathoState = "Tumor"
+    if material == "1" or material == "2" or material == "3" or material == "4" or material == "5":
+        sample.bioSpeciType = "Frozen Tissue"
+        sample.storCond = "Cryotube 1–2mL Programmable freezing to &lt;-135°C"
+    if material == "53" or material == "54" or material == "55" or material == "56":
+        sample.bioSpeciType = "Cryopreserved Tissue"
+    if material == "7":
+        sample.bioSpeciType = "Cell Pellet"
+        sample.storCond = "Cryotube 1–2mL Programmable freezing to &lt;-135°C"
+    if material == "C" or material == "K" or material == "L" or material == "PD" or material == "SD" or material == "T":
+        sample.bioSpeciType = "Serum or Plasma"
+        sample.storCond = "Cryotube 1–2mL Programmable freezing to &lt;-135°C"
+    if material == "gD":
+        sample.bioSpeciType = "Blood DNA"
+        sample.storCond = "PP tube 0.5–2mL (-35) to (-18)°C"
+    if material == "PK":
+        sample.bioSpeciType = "Peripheral Blood"
+        sample.storCond = "PP tube 0.5–2mL (-35) to (-18)°C"
+    if material == "PR":
+        sample.bioSpeciType = "Tumor Cell Line"
+        sample.storCond = "Cryotube 1–2mL Programmable freezing to &lt;-135°C"
+    sample.diagnosis = data.get('Samples')[0].get('diagnosis')
     return sample
 
 
